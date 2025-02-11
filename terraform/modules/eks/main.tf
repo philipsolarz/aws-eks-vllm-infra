b2b7a8417cb83cd7f5ffe45f03ef72fc00634038
Philip Solarz <philipsolarz@outlook.com>
2025-02-11 20:15:57 +0100

    Remove AWS Load Balancer Controller IAM policy and role attachment from EKS module

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -66,15 +66,6 @@ resource "aws_iam_role" "aws_lbc" {
   assume_role_policy = data.aws_iam_policy_document.aws_lbc.json
 }
 
-resource "aws_iam_policy" "aws_lbc" {
-  policy = file("${path.module}/policies/AWSLoadBalancerController.json")
-  name   = "AWSLoadBalancerController"
-}
-
-resource "aws_iam_role_policy_attachment" "aws_lbc" {
-  policy_arn = aws_iam_policy.aws_lbc.arn
-  role       = aws_iam_role.aws_lbc.name
-}
 
 resource "aws_eks_pod_identity_association" "aws_lbc" {
   cluster_name    = module.eks_cluster.cluster_name
